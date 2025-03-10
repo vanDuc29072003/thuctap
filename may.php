96ce439efff8cf28b3e53d28911ac4f897ff4631
vanDuc29072003 <phamduc290703@gmail.com>
2025-03-10 11:32:22 +0700

    fix: remove duplicate charset meta tag and update logout link to login page

--- may.php
+++ may.php
@@ -17,7 +17,6 @@
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta charset="UTF-8"/>
     <title>CTY TNHH IN T.KHOA</title>
     <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
     <link rel="icon" href="assets/img/logo.png" type="image/x-icon" />
@@ -165,7 +164,7 @@
                 </div>
               </li>
               <li class="nav-item ">
-                <a href="logout.php" class="collapsed" aria-expanded="false">
+                <a href="login.html" class="collapsed" aria-expanded="false">
                   <i class="fa-solid fa-right-from-bracket"></i>
                   <p>Đăng xuất</p>
                 </a>
