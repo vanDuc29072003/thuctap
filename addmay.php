2184d7ceb202cce06075877f0c125447c159f5d0
vanDuc29072003 <phamduc290703@gmail.com>
2025-03-10 11:23:28 +0700

    update may

--- addmay.php
+++ addmay.php
@@ -205,53 +205,10 @@
             <table class="table table-bordered">
               <div class="d-flex justify-content-between align-items-center mb-3">
                 <h1 class="mb-0">Danh sách máy</h1>
-                <a href="addmay.php">
                 <button class="btn btn-primary">
                   <i class="fa fa-plus"></i> Thêm mới
                 </button>
-                </a>
               </div>              
-              <div class="table-responsive">
-                <table class="table table-bordered">
-                  <thead style="background-color: pink; color: black;">
-                    <tr>
-
-                      <th>STT</th>
-                      <th>Tên máy</th>
-                      <th>Seri máy</th>
-                      <th>Chu kì bảo trì</th>
-                      <th>Năm sản xuất</th>
-                      <th>Hãng sản xuất</th>
-                      <th>Thao tác</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      <?php foreach ($result as $items):?>
-                      <tr>
-                         <td><?php echo $items['MaMay'] ?></td> 
-                         <td><?php echo $items['TenMay'] ?></td> 
-                          <td><?php echo $items['SeriMay'] ?></td>  
-                           <td><?php echo $items['ChuKyBaoTri'] ?></td> 
-                            <td><?php echo $items['NamSanXuat'] ?></td> 
-                             <td><?php echo $items['HangSanXuat'] ?></td> 
-                             <td>
-                        
-                        <div class="d-flex gap-2">
-                          <button class="btn btn-warning btn-sm">
-                              <i class="fa fa-edit"></i> Sửa
-                            </button>
-                          <button class="btn btn-danger btn-sm">
-                            <i class="fa fa-trash"></i> Xóa
-                          </button>
-                        </div>
-
-                    </td>
-                      </tr>
-                    <?php endforeach ?>       
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
             </div>
             </table>
           </div>
